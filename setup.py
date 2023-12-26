4100950c6ab9fb5a4e39ac0b422d60b68f7113bb
B4PT0R <bferrand.maths@gmail.com>
2023-12-26 17:19:54 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="objdict",
+    name="objdict-bf",
     version="0.1.0",
     author="Baptiste Ferrand",
     author_email="bferrand.maths@gmail.com",
